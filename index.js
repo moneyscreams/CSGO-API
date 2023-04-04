01ccc6b5cfa3ac54f1355a531af42224352f094f
moneyscreams <107195826+moneyscreams@users.noreply.github.com>
2023-04-04 20:57:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const { Storage } = require('megajs')
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(require("./res/index.json"))
+  res.send("API IS AWAKE, WAITING FOR REQUESTS")
 });
 
-app.get('/tldr', (req, res) => {
+app.get('/check', (req, res) => {
   var id = req.query.id
   res.send('Hello Express app!' + id)
 
